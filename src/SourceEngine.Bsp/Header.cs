dd16bbffe22849aab401d158f075b3b46fc47bd2
MarkKoz <KozlovMark@gmail.com>
2021-04-22 20:38:15 -0700

    Add struct constructors that use BinaryReader

--- src/SourceEngine.Bsp/Header.cs
+++ src/SourceEngine.Bsp/Header.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace SourceEngine.Bsp
 {
     internal struct Header
@@ -6,5 +8,17 @@ namespace SourceEngine.Bsp
         public int Version; // BSP file version
         public Lump[] Lumps; // Lump directory array
         public int MapRevision; // Map's version/revision number
+
+        public Header(BinaryReader reader)
+        {
+            Identifier = reader.ReadInt32();
+            Version = reader.ReadInt32();
+            Lumps = new Lump[64];
+
+            for (int lump = 0; lump < Lumps.Length; ++lump)
+                Lumps[lump] = new Lump(reader);
+
+            MapRevision = reader.ReadInt32();
+        }
     }
 }
--- src/SourceEngine.Bsp/Lump.cs
+++ src/SourceEngine.Bsp/Lump.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace SourceEngine.Bsp
 {
     internal struct Lump
@@ -6,5 +8,16 @@ namespace SourceEngine.Bsp
         public int FileLength; // Length of lump (bytes)
         public int Version; // Lump format version
         public byte[] FourCC; // Lump identifier code
+
+        public Lump(BinaryReader reader)
+        {
+            FileOffset = reader.ReadInt32();
+            FileLength = reader.ReadInt32();
+            Version = reader.ReadInt32();
+            FourCC = new byte[4];
+
+            for (int i = 0; i < FourCC.Length; ++i)
+                FourCC[i] = reader.ReadByte();
+        }
     }
 }
