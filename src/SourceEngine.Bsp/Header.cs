6015c6caec5a26abb2a6436589c2bcc9d9387193
MarkKoz <KozlovMark@gmail.com>
2021-04-22 20:44:25 -0700

    Validate the header identifier and version

--- src/SourceEngine.Bsp/Header.cs
+++ src/SourceEngine.Bsp/Header.cs
@@ -1,9 +1,14 @@
+using System;
 using System.IO;
 
 namespace SourceEngine.Bsp
 {
     internal struct Header
     {
+        private const int IDENTIFIER = ('P' << 24) + ('S' << 16) + ('B' << 8) + 'V';
+        private const int MIN_BSP_VERSION = 19;
+        private const int BSP_VERSION = 21;
+
         public int Identifier; // BSP file identifier
         public int Version; // BSP file version
         public Lump[] Lumps; // Lump directory array
@@ -12,9 +17,14 @@ namespace SourceEngine.Bsp
         public Header(BinaryReader reader)
         {
             Identifier = reader.ReadInt32();
-            Version = reader.ReadInt32();
-            Lumps = new Lump[64];
+            if (Identifier != IDENTIFIER)
+                throw new Exception("Invalid BSP header identifier.");
 
+            Version = reader.ReadInt32();
+            if (Version is < MIN_BSP_VERSION or > BSP_VERSION)
+                throw new Exception($"Map has incorrect BSP version ({Version} should be {BSP_VERSION}).");
+
+            Lumps = new Lump[64];
             for (int lump = 0; lump < Lumps.Length; ++lump)
                 Lumps[lump] = new Lump(reader);
 
